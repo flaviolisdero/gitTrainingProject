6910e76aac5988a7d85b9b1401f8c77388374d4c
Khouloud Zayati <kzayati@adflux.net>
2024-09-23 10:01:53 +0200

    first commit

---
+++ name_displayer.py
@@ -0,0 +1,5 @@
+filename = 'C:\Users\kzayati\OneDrive - Business & Decision Europe\Desktop\git FORMATION\name_displayer.py'
+
+# Open the file in read mode
+with open(filename, 'r') as file:
+    print(f"khouloud zayati")
