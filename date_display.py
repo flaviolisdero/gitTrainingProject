c16947c0de2c2b7d990fa68aa0e1f845cccf3ba1
Khouloud Zayati <kzayati@adflux.net>
2024-09-23 10:30:55 +0200

    second commit date

---
+++ date_display.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+# Get the current date
+current_date = datetime.now()
+
+# Print the current date in a readable format
+print("Current date:", current_date.strftime("%Y-%m-%d"))
